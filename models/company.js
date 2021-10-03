d726d0cb16512710357b7773bd380bcca45b1173
Jesse Brink <jbrink0925@gmail.com>
2021-10-03 00:47:36 -0400

    add the ability to filter queries in the 'findAll()' method in 'company.js', and pass the request query into findAll in the GET /companies route. Add documentation on changes as well.

--- models/company.js
+++ models/company.js
@@ -7,6 +7,23 @@ const { sqlForPartialUpdate } = require("../helpers/sql");
 /** Related functions for companies. */
 
 class Company {
+  /**
+   * Helper function that is used to format the request query string used in the 
+   * database query's WHERE clause in the "findAll" method. 
+   * 
+   * key: a key from request.query, but also the name of a column in the "companies" table
+   * idx: an index, used to prevent SQL injects with statements such as "WHERE name = $1"
+   * 
+   * return: No return; modifies the array member in place
+   */
+
+  static makeKeyStatementsForWhereClauses(key, idx) {
+    console.log("hello!")
+    if (key === "name") return ` ${key} ILIKE $${idx+1}`
+    if (key === "minEmployees") return `num_employees >= $${idx+1}`
+    if (key === "maxEmployees") return `num_employees <= $${idx+1}`
+  }
+
   /** Create a company (from data), update db, return new company data.
    *
    * data should be { handle, name, description, numEmployees, logoUrl }
@@ -47,18 +64,56 @@ class Company {
   /** Find all companies.
    *
    * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
+   * 
+   * Accepts a query string: parameters are "name", "minEmployees", and "maxEmployees"
+   * if any parameters are given outside of those three terms, the method will throw a
+   * BadRequestError. 
+   * 
+   * name: the name of a company
+   * minEmployees: the minimum number of employees a company will have
+   * maxEmployees: the maximum number of employees a company will have
+   * 
+   * One query is made for instances when there is no query string, and another query is 
+   * made to reflect the query string, meaning that the database query will only look for the values
+   * that were present in the query string itself; not all three possible values (unless all three were given).
+   * 
    * */
 
-  static async findAll() {
-    const companiesRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           ORDER BY name`);
-    return companiesRes.rows;
+  static async findAll(query = null) {
+    const validParams = ["name", "minEmployees", "maxEmployees"];
+    const keys = Object.keys(query);
+    const invalidKeys = keys.filter(k => validParams.indexOf(k) === -1);
+    if (invalidKeys.length) throw new BadRequestError(`These parameters in your query 
+                                                       string are invalid: [${invalidKeys}]`);
+    
+    if (!query) {
+      const companiesRes = await db.query(
+        `SELECT handle,
+                name,
+                description,
+                num_employees AS "numEmployees",
+                logo_url AS "logoUrl"
+         FROM companies
+         ORDER BY name`);
+      return companiesRes.rows;
+    } else {
+      
+      let queryKeys = keys.map((key, idx) => this.makeKeyStatementsForWhereClauses(key, idx)).join(" AND ");
+      if (query.name) {
+        query.name = `%${query.name}%`
+      }
+      const values = Object.values(query);
+      const companiesRes = await db.query(
+        `SELECT handle,
+                name,
+                description,
+                num_employees AS "numEmployees",
+                logo_url AS "logoUrl"
+         FROM companies
+         WHERE ${queryKeys}
+         ORDER BY name`, [...values]);
+      return companiesRes.rows;
+    }
   }
 
   /** Given a company handle, return data about company.
